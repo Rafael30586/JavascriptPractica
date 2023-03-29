955a80f1ef9efa4cc37c6e0116f33dc35770e497
F. Rafael Alvarez <fraq86@gmail.com>
2023-03-29 15:40:17 -0300

    primer commit

---
+++ codigo.js
@@ -0,0 +1,9 @@
+let animal = document.getElementById("rinoceronte");
+let despedida = document.getElementById("chau");
+let botonGrande = document.getElementById("botonaso");
+
+function cambiarColor(){
+	alert("Hola");
+}
+
+botonGrande.addEventListener('click',cambiarColor);
