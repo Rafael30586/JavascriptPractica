036345af8cfdaaeb0f2ef5198060e6809111a16c
F. Rafael Alvarez <fraq86@gmail.com>
2023-11-05 11:24:54 -0300

    Insercion de elementos en contenedor

--- Practica4/codigo.js
+++ Practica4/codigo.js
@@ -2,11 +2,12 @@ const contenedor = document.getElementById("contenedor");
 let elementos = [];
 let h1;
 let texto;
+let tamanio = 12;
 
 
 
 function crearElementos(){
-    for(let i=0;i<12;i++){
+    for(let i=0;i<tamanio;i++){
         h1 = document.createElement("h1");
         elementos.push(h1);
         
@@ -31,7 +32,14 @@ function crearElementos(){
     }
 }
 
+function insertarElementos(){
+    for(let i=0;i<tamanio;i++){
+        contenedor.appendChild(elementos[i]);
+    }
+}
+
 crearElementos();
+insertarElementos();
 
 console.log(elementos);
 
