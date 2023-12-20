add8866e5d4bc6a2c990da3cc6c1d17f805bf389
F. Rafael Alvarez <fraq86@gmail.com>
2023-12-20 08:38:03 -0300

    Prueba con inputs radio y check

--- ElementosHTML/codigo.js
+++ ElementosHTML/codigo.js
@@ -1,12 +1,16 @@
 let boton = document.getElementById("boton");
-let color = document.getElementById("color");
+let color1 = document.getElementById("color1");
+/*let color2 = document.getElementById("color2");
+let color3 = document.getElementById("color3");
+let color4 = document.getElementById("color4");
+let color5 = document.getElementById("color5");*/
 let body = document.getElementById("body");
 
 function colorearFondo(){
-    body.style.backgroundColor = color.value;
+    body.style.backgroundColor = color1.value;
 }
 
-console.log(color);
-console.log(color.value);
+console.log(color1);
+console.log(color1.value);
 
 boton.addEventListener("click",colorearFondo);
