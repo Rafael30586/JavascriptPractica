a7cbc5cea2fdbbe7cc58a163a987f27b9014232e
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-09 09:59:24 -0300

    Obtencion de JSON a partir de elementosHTML

--- PersonajesDeFiccion_I3/EdicionDeObras/edicion_obras_codigo.js
+++ PersonajesDeFiccion_I3/EdicionDeObras/edicion_obras_codigo.js
@@ -1,7 +1,24 @@
 let botonEnviar = document.getElementById("botonEnviar");
+let obraSeleccionada = document.getElementById("selectObra"); //con .value accedo al elemento seleccionado
+let inputTitulo = document.getElementById("inputTitulo"); //.value
+let inputLanzamiento = document.getElementById("inputLanzamiento"); //.valueAsNumber
+let selectClasificacion = document.getElementById("selectClasificacion"); //value
+let obraObjeto = {};
+let obraJSON;
 
 function saludar(){
     alert("Hola mundo");
 }
+
+function armarJSON(){
+    obraObjeto = {
+    id:0,titulo:inputTitulo.value,anioLanzamiento:inputLanzamiento.valueAsNumber,
+    clasificacion:selectClasificacion.value,   
+    }
+
+    obraJSON = JSON.stringify(obraObjeto);
+}
+
+armarJSON();
 
 botonEnviar.addEventListener("click",saludar);
