ad4892162cc1a6e6fad48d1c3372c4dfdc18af3e
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-15 18:26:26 -0300

    Practica con prototipos

---
+++ Prototipos/codigo.js
@@ -0,0 +1,26 @@
+let numero = 65;
+let cadena = "Auto";
+let posta = true;
+let arreglo = [3,68,false,"que"];
+
+function vender(){
+    alert("Hola che");
+
+}
+
+console.log(numero.__proto__);
+console.log(cadena.__proto__);
+console.log(posta.__proto__);
+console.log(arreglo.__proto__);
+console.log(arreglo.__proto__.__proto__);
+console.log(vender.__proto__);
+console.log(vender.prototype);
+console.log(vender.prototype.__proto__);
+
+
+
+
+console.log(numero.toFixed());
+console.log(cadena.blink());
+console.log(posta);
+console.log(posta.toString());
