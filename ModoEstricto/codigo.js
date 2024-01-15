8633304273bcc07624e9a0884b392b6d24dcb3f4
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-15 18:35:40 -0300

    Practica con el strict mode

---
+++ ModoEstricto/codigo.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+//let a; //si no se declara la variable se produce un error que aparece en la consola, esto es debido al...
+a = 3;//... "use strict" 
+
+let b = 9
+
+console.log(a);
+console.log(b) //el strict mode no exije punto y coma
