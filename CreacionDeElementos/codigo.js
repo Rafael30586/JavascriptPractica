13f49761862f4bb515d7028477f599c3c82b6fe3
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-16 09:37:35 -0300

    Dos formas de crear elementos html

--- CreacionDeElementos/codigo.js
+++ CreacionDeElementos/codigo.js
@@ -1,8 +1,13 @@
 let div = '<div class="divCreado"></div>'; 
+let boton = '<button id="botonCreado"></button>';
 let contenedor = document.querySelector(".contenedor");
+let contenedor2 = document.querySelector(".caja");
+let input = document.createElement("input");
 
 console.log(div);
 console.log(contenedor);
 
 contenedor.innerHTML = div;
+//contenedor.outerHTML = boton;
+contenedor2.appendChild(input);
 
