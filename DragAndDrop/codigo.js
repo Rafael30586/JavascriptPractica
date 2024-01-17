a2fbd0a4688233d614ea321684303f95fdeb7d90
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-17 19:33:12 -0300

    Dragenter y dragleave

--- DragAndDrop/codigo.js
+++ DragAndDrop/codigo.js
@@ -1,4 +1,5 @@
 let contenedor1 = document.getElementById("contenedor1");
+let contenedor2 = document.getElementById("contenedor2");
 
 function colorear(){
     contenedor1.style.backgroundColor = "#fdd96a";
@@ -15,4 +16,12 @@ contenedor1.addEventListener("dragend",()=>{
     contenedor1.style.backgroundColor = "#87c5b6";
     contenedor1.style.borderWidth = "10px";
     contenedor1.style.borderColor = "#05cc0c";
+});
+
+contenedor2.addEventListener("dragenter",()=>{
+    contenedor2.setAttribute("class",contenedor1.getAttribute("class"));
+});
+
+contenedor2.addEventListener("dragleave",()=>{ //pareece que no esta funcionando
+    contenedor2.setAttribute("class","claseCualquiera");
 });
