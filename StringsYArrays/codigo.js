15327510ff17530543d739ccb91a2d60648a6edd
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-18 00:18:28 -0300

    Practica con arrays y strings

---
+++ StringsYArrays/codigo.js
@@ -0,0 +1,24 @@
+let arr = [67,"Computadora",true,57.34,'s',false,0.54,true,"Que"];
+let arr2 = [true,67,'s'];
+
+let elemento = arr.at(3);
+let cadena = arr.join(" - ");
+let ultimo = arr.pop();
+arr.push("Hola che");
+let pedazo = arr.shift();
+arr.unshift(302);
+let arr3 = arr.concat(arr2);
+
+
+
+console.log(elemento);
+console.log(cadena);
+console.log(ultimo);
+console.log(pedazo);
+console.log("Nuevo array: "+ arr);
+console.log(arr3);
+
+
+
+
+//console.log()
