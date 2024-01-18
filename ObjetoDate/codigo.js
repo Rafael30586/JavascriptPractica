fb5bb02d14056b7c7b472e45c0b065382cdcde80
F. Rafael Alvarez <fraq86@gmail.com>
2024-01-17 23:51:50 -0300

    Practica con objeto Date

---
+++ ObjetoDate/codigo.js
@@ -0,0 +1,16 @@
+let fecha = new Date();
+
+console.log(fecha.getDate());
+console.log(fecha.getDay());
+console.log(fecha.getFullYear());
+console.log(fecha.getMinutes());
+console.log(fecha.getMonth());
+console.log(fecha.getHours());
+console.log(fecha.getMilliseconds());
+
+
+
+
+
+
+//console.log();
