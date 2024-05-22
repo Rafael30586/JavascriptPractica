e76c220f72099f4059631412cc830c9331e5f785
F. Rafael Alvarez <fraq86@gmail.com>
2024-05-22 19:39:45 -0300

    Imagen de fondo

--- Portafoilo5ComponentesPrototipo/Ventanas/codigo.js
+++ Portafoilo5ComponentesPrototipo/Ventanas/codigo.js
@@ -3,21 +3,26 @@ let tema = 0;
 
 boton.addEventListener('click',()=>{
 
-    if(tema==1){
+    if(tema==0){
         document.documentElement.style.setProperty('--color-ventana-exterior','#570007');
         document.documentElement.style.setProperty('--color-ventana-interior','#FF0A1F');
         document.documentElement.style.setProperty('--color1','#FFCCD0');
         document.documentElement.style.setProperty('--color2','#FF858F');
-    }else if(tema==2){
+    }else if(tema==1){
         document.documentElement.style.setProperty('--color-ventana-exterior','#021400');
         document.documentElement.style.setProperty('--color-ventana-interior','#58FF42');
         document.documentElement.style.setProperty('--color1','#D2FFCC');
         document.documentElement.style.setProperty('--color2','#6FFF5C');
-    }else if(tema==3){
+    }else if(tema==2){
         document.documentElement.style.setProperty('--color-ventana-exterior','#272900');
         document.documentElement.style.setProperty('--color-ventana-interior','#F9FF42');
         document.documentElement.style.setProperty('--color1','#FDFFB8');
         document.documentElement.style.setProperty('--color2','#FAFF5C');
+    }else if(tema==3){
+        document.documentElement.style.setProperty('--color-ventana-exterior','#00031F');
+        document.documentElement.style.setProperty('--color-ventana-interior','#000FB8');
+        document.documentElement.style.setProperty('--color1','#D6DAFF');
+        document.documentElement.style.setProperty('--color2','#6673FF');
     }else if(tema==4){
         document.documentElement.style.setProperty('--color-ventana-exterior','#570007');
         document.documentElement.style.setProperty('--color-ventana-interior','#FF0A1F');
@@ -28,16 +33,11 @@ boton.addEventListener('click',()=>{
         document.documentElement.style.setProperty('--color-ventana-interior','#FF0A1F');
         document.documentElement.style.setProperty('--color1','#FFCCD0');
         document.documentElement.style.setProperty('--color2','#FF858F');
-    }else if(tema==6){
-        document.documentElement.style.setProperty('--color-ventana-exterior','#570007');
-        document.documentElement.style.setProperty('--color-ventana-interior','#FF0A1F');
-        document.documentElement.style.setProperty('--color1','#FFCCD0');
-        document.documentElement.style.setProperty('--color2','#FF858F');
     }
-    if(tema<6){
+    if(tema<4){
         tema++;
     }else{
-        tema=1;
+        tema=0;
     }
 
 });
