ef23ca8a45aa034da9c8d4f84f1002511c2d8c66
F. Rafael Alvarez <fraq86@gmail.com>
2024-05-24 19:41:06 -0300

    Carrousel incompleto pero exitoso

--- Portafoilo5ComponentesPrototipo/CarrouselZIndex/codigo.js
+++ Portafoilo5ComponentesPrototipo/CarrouselZIndex/codigo.js
@@ -1,2 +1,20 @@
 let patras = document.getElementById("patras");
 let palante = document.getElementById("palante");
+let tema = 0;
+
+palante.addEventListener('click',()=>{
+    if(tema==0){
+        document.documentElement.style.setProperty('--zindex1','30');
+    }else if(tema==1){
+        document.documentElement.style.setProperty('--zindex2','40');
+    }else if(tema==2){
+        document.documentElement.style.setProperty('--zindex3','50');
+    }else if(tema==3){
+        document.documentElement.style.setProperty('--zindex4','60');
+    } 
+    if(tema<4){
+        tema++;
+    }else{
+        tema==0;
+    }
+})
