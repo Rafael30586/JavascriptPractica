dea88d851ef2554af2d8466979b9bbeb6b10a260
F. Rafael Alvarez <fraq86@gmail.com>
2024-06-22 21:30:08 -0300

    Coloracion de boton con eventos de teclado

--- EventosDeTeclado/codigo.js
+++ EventosDeTeclado/codigo.js
@@ -5,4 +5,21 @@ document.addEventListener('keydown', e =>{
     console.log(e);
     console.log(e.code);
     console.log(e.key);
+    /*
+    switch(e.code){
+        case "KeyR": boton.setAttribute("color","rojo");break;
+        case "KeyA": boton.setAttribute("color","azul");break;
+        case "KeyP": boton.setAttribute("color","rosa");break;
+        case "KeyV": boton.setAttribute("color","verde");break;           
+    }*/
+    if(e.code=="KeyR"){
+        boton.setAttribute("color","rojo");
+    }else if(e.code=="KeyA"){
+        boton.setAttribute("color","azul");
+    }else if(e.code=="KeyP"){
+        boton.setAttribute("color","rosa");
+    }else if(e.code=="KeyV"){
+        boton.setAttribute("color","verde");
+    }
 })
+
