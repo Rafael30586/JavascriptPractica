d6c52ec580ee0c3708c119adf397d0bd1c522d24
F. Rafael Alvarez <fraq86@gmail.com>
2024-06-27 21:41:25 -0300

    Prueba exitosa con API de Rick and Morty

--- ConexionConAPIs/codigo.js
+++ ConexionConAPIs/codigo.js
@@ -1,16 +1,37 @@
 let objeto = [];
+let contenedorPersonaje = document.getElementById("contenedor-personaje");
+let nombre = document.getElementById("nombre");
+let imagen = document.getElementById("imagen");
+let numeroPersonaje = Math.round(Math.random()*18);
+let pagina = Math.round(Math.random()*41);
 
 
 async function mostrarPersonajes(){
-    await fetch(`https://rickandmortyapi.com/api/character/?page=3`)
+    await fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
         .then((response) => response.json())  
 	    .then((personajes) => {
             objeto = Object.values(personajes);
             return objeto;
         }).then((objeto) => {
-            return personaje = objeto[1][12];
+            return personaje = objeto[1][numeroPersonaje];
         }).then((personaje)=>console.log(personaje));
     
 }
 
-mostrarPersonajes();
+//mostrarPersonajes();
+
+async function mostrarPersonaje(){
+    await fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
+        .then((response) => response.json())  
+	    .then((personajes) => {
+            objeto = Object.values(personajes);
+            return objeto;
+        }).then((objeto) => {
+            personaje = objeto[1][numeroPersonaje];
+            nombre.textContent = personaje.name;
+            imagen.setAttribute("src",personaje.image);
+            return personaje
+        }).then((personaje)=>console.log(personaje)); 
+}
+
+mostrarPersonaje();
