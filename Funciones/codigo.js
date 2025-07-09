ef4225a96e1bf0e19242363688471f9afe249265
Rafael305 <fraq86@gmail.com>
2025-07-09 10:27:10 -0300

    funciones flecha

--- Funciones/codigo.js
+++ Funciones/codigo.js
@@ -14,3 +14,21 @@ function returnGreeting(){ // Una función puede retornar otra función
         console.log("Hello")
     }
 }
+
+// Funciones flecha
+
+const multiply = (a,b) => a * b // A esto se le llama retorno implícito
+console.log(multiply(16,2))
+
+const handler = {
+    name: "Link",
+    greeting: function(){
+        console.log(`Hola ${this.name}`) // A esto se le llama this léxico
+    },
+    arrowGreeting: ()=>{
+        console.log(`Hola ${this.name}`) // En este caso no funciona porque la función flecha crea su propio contexto
+    }
+}
+
+handler.greeting()
+handler.arrowGreeting()
