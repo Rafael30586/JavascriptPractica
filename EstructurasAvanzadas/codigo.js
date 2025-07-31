4c3addf4e3549bcf79fd2131376de89a85cba64d
Rafael305 <fraq86@gmail.com>
2025-07-31 09:36:00 -0300

    ejercicios 1 y 2 de estructuras avanzadas

--- EstructurasAvanzadas/codigo.js
+++ EstructurasAvanzadas/codigo.js
@@ -146,4 +146,42 @@ console.log(objectFromMap)
 const mapFromObject = new Map(Object.entries(objectFromMap))
 console.log(mapFromObject)
 
+// 1. Utiliza map, filter y reduce para crear un ejemplo diferente al de la lección
+
+// 2. Dado un array de números, crea uno nuevo con dichos números elevados al cubo y filtra sólo los números pares
+
+// 3. Utiliza flat y flatMap para crear un ejemplo diferente al de la lección
+
+// 4. Ordena un array de números de mayor a menor
+
+// 5. Dados dos sets, encuentra la unión, intersección y diferencia de ellos
+
+// 6. Itera los resultados del ejercicio anterior
+
+// 7. Crea un mapa que almacene información se usuarios (nombre, edad y email) e itera los datos
+
+// 8. Dado el mapa anterior, crea un array con los nombres
+
+// 9. Dado el mapa anterior, obtén un array con los email de los usuarios mayores de edad y transfórmalo a un set
+
+// 10. Transforma el mapa en un objeto, a continuación, transforma el objeto en un mapa con clave el email de cada usuario y como valor todos los datos del usuario
+
+/*Ejercicio 1 */
+
+let palabras = ["manzana", "coche", "cielo", "árbol", "mar"]
+let palabrasEnMayuscula = palabras.map(palabra=>palabra.toUpperCase())
+console.log(palabrasEnMayuscula)
+
+let alimentos = ["queso","orégano","sal","naranja","nuez","tomate"]
+let alimentosQueEmpiezanConN = alimentos.filter(alimento=>alimento.startsWith("n"))
+console.log(alimentosQueEmpiezanConN)
+
+let cadenaDeAlimentos = alimentos.reduce((previous,current)=>current.concat(previous))
+console.log(cadenaDeAlimentos)
+
+// Ejercicio 2
+
+let numeros = [32,10,71,20,45]
+let numerosElevadosAlCubo = numeros.map(numero=>numero*numero*numero)
+let numerosElevadosAlCuboPares = numerosElevadosAlCubo.filter(numero=>numero % 2 === 0)
 
